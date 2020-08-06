364d4ca63e658c6f705f8d0f5b0e91cdce5b913f
Vinnie Lopez <vinnielo01@gmail.com>
2020-08-06 14:35:52 -0700

    Updates

---
+++ precache-manifest.105b2a8d62ea74300dda8c60084b666e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c96da14aeffbd7c2e6b53e5aab4881a",
+    "url": "/thescreensquadpodcast.com/index.html"
+  },
+  {
+    "revision": "c50a3ece5640d3b60700",
+    "url": "/thescreensquadpodcast.com/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "ae7e6e78460a569edc32",
+    "url": "/thescreensquadpodcast.com/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c50a3ece5640d3b60700",
+    "url": "/thescreensquadpodcast.com/static/js/2.e0db9f35.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/thescreensquadpodcast.com/static/js/2.e0db9f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae7e6e78460a569edc32",
+    "url": "/thescreensquadpodcast.com/static/js/main.cb51332c.chunk.js"
+  },
+  {
+    "revision": "cb79069008fbcdb93ad9",
+    "url": "/thescreensquadpodcast.com/static/js/runtime-main.736a8056.js"
+  },
+  {
+    "revision": "25d5b23203e9d753da433d4568520566",
+    "url": "/thescreensquadpodcast.com/static/media/video-1596665337.25d5b232.mp4"
+  }
+]);
